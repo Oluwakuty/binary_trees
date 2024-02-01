09d5fc023bea2586edde4563cdc43cc62916cea9
oluwakuty <oyindamoanu@gmail.com>
2024-02-01 10:54:03 -0800

    corrected task one

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,18 +16,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newer;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-
-	if (new_node == NULL)
+	newer = binary_tree_node(parent, value);
+	if (newer == NULL)
 		return (NULL);
+
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		newer->left = parent->left;
+		parent->left->parent = newer;
 	}
-	parent->left = new_node;
-	return (new_node);
+	parent->left = newer;
+
+	return (newer);
 }
