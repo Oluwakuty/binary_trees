2bf4133fa0a0afe69ab8c87a05d1740da714eb76
oluwakuty <oyindamoanu@gmail.com>
2024-02-01 10:43:33 -0800

    binary task one corrected

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node = create_binary_tree_node(parent, value);
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
@@ -30,3 +30,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	parent->left = new_node;
 	return (new_node);
+}
