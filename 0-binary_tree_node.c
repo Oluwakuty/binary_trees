7bb739b4d72ce88cd505a7379ed77395f8c99027
oluwakuty <oyindamoanu@gmail.com>
2024-02-01 10:20:38 -0800

    binary zero task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,14 +7,20 @@
  *
  * Return: A pointer to the new node, or NULL on failure.
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(*new));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (!new)
+	if (!new_node)
+	{
 		return (NULL);
+	}
 
-	*new = (binary_tree_t){.n = value, .parent = parent,
-		.left = NULL, .right = NULL};
-	return (new);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
